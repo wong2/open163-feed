fd20b0252a75085df2acb731096a9c6b8354dd15
wong2 <wonderfuly@gmail.com>
2023-07-22 18:28:28 +0800

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+deno run --allow-net hello.ts
+```
---
+++ main.ts
@@ -0,0 +1,48 @@
+import { Hono } from "https://deno.land/x/hono@v3.3.1/mod.ts";
+import { Podcast } from "https://esm.sh/podcast@2.0.1";
+
+async function fetchCourse(plid: string) {
+  const resp = await fetch(`https://c.open.163.com/open/mob/movie/list.do?plid=${plid}`);
+  const json = await resp.json();
+  return json.data;
+}
+
+const app = new Hono();
+
+app.get("/:plid", async (c) => {
+  const plid = c.req.param("plid");
+  const course = await fetchCourse(plid);
+
+  const feed = new Podcast({
+    title: course.title,
+    description: course.description,
+    siteUrl: course.pageUrl,
+    imageUrl: course.imgPath,
+    copyright: course.school,
+    author: course.director,
+  });
+
+  for (const video of course.videoList) {
+    feed.addItem({
+      title: video.title,
+      description: video.description,
+      url: video.webUrl,
+      guid: video.mid,
+      date: new Date(),
+      enclosure: {
+        url: video.mp4SdUrl,
+      },
+      itunesImage: video.imgPath,
+    });
+  }
+
+  const xml = feed.buildXml();
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/rss+xml;charset=UTF-8",
+    },
+  });
+});
+
+Deno.serve(app.fetch);
